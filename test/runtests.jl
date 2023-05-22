404526cc2ee601db6d563355827efcadf9b7f18e
Jeffrey Varner <jdv27@cornell.edu>
2023-05-22 16:44:24 -0400

    What the hell is going on?
    wow

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,7 +1,11 @@
 using VLOrderBook
-using Test, Random, Dates
-using AVLTrees: AVLTree
-using Base.Iterators: zip,cycle,take,filter
+using Test
+using Random, Dates
+using Base.Iterators: cycle, take, zip, flatten
+
+# do we actually need these?
+# using AVLTrees: AVLTree
+# using Base.Iterators: zip, cycle, take, filter
 
 @testset "VLOrderBook.jl" begin
     include("./test-logic.jl")
--- test/test-logic.jl
+++ test/test-logic.jl
@@ -1,13 +1,13 @@
 begin # Create (Deterministic) Limit Order Generator
+    
     MyOrderSubTypes = (Int64,Float64,Int64,Int64) # define types for Order Size, Price, Order IDs, Account IDs
     MyOrderType = Order{MyOrderSubTypes...}
     MyLOBType = OrderBook{MyOrderSubTypes...}
     MyUOBType = UnmatchedOrderBook{Int64, Float64, Int64, Int64, DateTime, String, Integer}
 
-    using Base.Iterators: cycle, take, zip, flatten
     orderid_iter = Base.Iterators.countfrom(1)
     sign_iter = cycle([1,-1,-1,1,1,-1])
-    side_iter = ( s>0 ? SELL_ORDER : BUY_ORDER for s in sign_iter )
+    side_iter = ( s > 0 ? SELL_ORDER : BUY_ORDER for s in sign_iter)
     spread_iter = cycle([3 2 3 2 2 2 3 2 3 4 2 2 3 2 3 2 3 3 2 2 3 2 5 2 2 2 2 2 4 2 3 6 5 6 3 2 3 5 4]*1e-2)
     price_iter = ( Float32(100.0 + sgn*δ) for (δ,sgn) in zip(spread_iter,sign_iter) )
     size_iter = cycle([2,5,3,4,10,15,1,6,13,11,4,1,5])
